ab51795af00372e446602300606df1a1bc0843e8
Ravi <ravimajithiya.techeniac@gmail.com>
2022-02-15 17:48:45 +0530

    basic_change

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,9 +38,9 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => false,
-                'host' => '127.0.0.1',
+                'host' => 'https://web-socket.techeniac.com',
                 'port' => 6001,
-                'scheme' => 'http'
+                'scheme' => 'https'
             ],
         ],
 
